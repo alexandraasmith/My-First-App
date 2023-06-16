42b2c4b517ff115ff997500266a2b195c7013c0d
Alexandra Smith <dralexandraasmith@gmail.com>
2023-06-16 10:37:11 -0500

    Initial Commit

---
+++ My First App/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  My First App
+//
+//  Created by Scholar on 6/7/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        VStack {
+            Text("Here lays some Classic Literature.")
+                .font(.title)
+        .foregroundColor(Color.black)
+            
+            
+        }
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ My First App/My_First_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_First_AppApp.swift
+//  My First App
+//
+//  Created by Scholar on 6/7/23.
+//
+
+import SwiftUI
+
+@main
+struct My_First_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
